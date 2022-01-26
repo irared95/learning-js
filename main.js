167c746eb352275244321bc2010c879f958c21de
Ira Redchets <irared95@gmail.com>
2022-01-26 14:28:16 +0200

    js arrays

--- main.js
+++ main.js
@@ -1,18 +1,95 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+// const cars = [
+//     [2, 2, 44],
+//     'BMW',
+//     'Toyota',
+//     'KIA',
+//     'AUDI',
+//     'Suzuki'
+// ]
+//     cars.length
+//     cars[1]= 'Lada'
+//     cars.push('Daewoo')
+//     cars.pop()
+//     cars.shift()
+//     console.log(cars[0][2])
+//     console.log(cars.length)
+//     console.log(cars[cars.length - 1])
+//     console.log(cars.push('Daewoo'))
+//     console.log(cars)
+//     console.log(cars.pop())
+//     console.log(cars.shift())
+//     const nameAdmin =[
+//         'Vladimir',
+//         'Irina',
+//         'John'
+//     ]
+//     console.log(nameAdmin)
+//     cars.concat(nameAdmin)
+//     console.log(cars.concat(nameAdmin))
+//     cars.indexOf('KIA')
+//     console.log(cars.indexOf('KIA'))
+//     cars.join(',')
+//     console.log(cars.join(','))
 
-    const string1 = 'hello world';
-    const string2 ='I like js';
-    let count = 0;
-    const h1= document.querySelector('.head');
+    const randomPhrase = [
+        'Have a nice day',
+        'Good job',
+        'You can do this',
+        'Вo not be lazy',
+    ]
+    const randomPhraseResult = randomPhrase[Math.floor(Math.random() * 4)];
+    console.log(randomPhraseResult)
 
-    setInterval(function () {
-        count++
-        if (count % 2){
-            h1.innerHTML=string1
-        }
-            else{
-            h1.innerHTML=string2
-        }
-    }, 1000);
+    const randomPartOfTheBody =[
+        'нога',
+        'губы',
+        'щеки',
+        'палец'
+    ]
+    const randomAdjective =[
+        'красивая',
+        'дурная',
+        'жирная'
+    ]
+    const randomAnimalBodyPart =[
+        'хвост',
+        'лапа',
+        'крыло'
+    ]
+    const randomAnimal =[
+        'лев',
+        'кот',
+        'щука',
+        'лось'
+    ]
+    const randomPartOfTheBodyResult = randomPartOfTheBody[Math.floor(Math.random() * randomPartOfTheBody.length)];
+    console.log(randomPartOfTheBodyResult)
+
+    const randomAdjectiveResult = randomAdjective[Math.floor(Math.random() * randomAdjective.length)];
+    console.log(randomAdjectiveResult)
+
+    const randomAnimalBodyPartResult = randomAnimalBodyPart[Math.floor(Math.random() * randomAnimalBodyPart.length)];
+    console.log(randomAnimalBodyPartResult)
+
+    const randomAnimalResult = randomAnimal[Math.floor(Math.random() * randomAnimal.length)];
+    console.log(randomAnimalResult)
+
+    const randomSentence = 'У тебя ' + randomPartOfTheBodyResult + ' еще более ' + randomAdjectiveResult + ' чем ' + randomAnimalBodyPartResult + ' у ' + randomAnimalResult;
+    console.log(randomSentence)
+
+    const randomSentenceResult = ['У тебя',randomPartOfTheBodyResult,'еще более',randomAdjectiveResult,'чем',randomAnimalBodyPartResult,'у',randomAnimalResult].join(' ')
+    console.log(randomSentenceResult)
+
+    ////
+
+    const num = [
+        3,
+        2,
+        1
+    ]
+    num.join('больше,чем')
+    console.log(num.join(' больше, чем '))
+
 });
