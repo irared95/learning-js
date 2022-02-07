f3c860093a96a95f688529fb0968edea754b43e9
Ira Redchets <irared95@gmail.com>
2022-02-07 22:02:19 +0200

    js function

--- main.js
+++ main.js
@@ -1,18 +1,97 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+
+// let drawCats = function (howManyTimes , whatToDraw){
+//     for (let i = 0; i < howManyTimes; i++){
+//         console.log(i + whatToDraw)
+//     }
+// }
+// drawCats(10, ' ^__^');
+
+// let double = function (number){
+//     return number * 2
+// }
+//
+//     console.log(double(3) + double(25))
+//     let pickRandomWord = function (words) {
+//         return words[Math.floor(Math.random() * words.length)];
+//     };
+//     let randomBodyParts = ["глаз", "нос", "череп"];
+//     let randomAdjectives = ["вонючая", "унылая", "дурацкая"];
+//     let randomWords = ["муха", "выдра", "дубина", "мартышка", "крыса"];
+//
+//     let randomInsult = "У тебя " + pickRandomWord(randomBodyParts) + " словно " + pickRandomWord(randomAdjectives) + ' ' + pickRandomWord(randomWords);
+//     console.log(randomInsult)
+
+    // let generateRandomInsult = function () {
+    //     let randomBodyParts = ["глаз", "нос", "череп"];
+    //     let randomAdjectives = ["вонючая", "унылая", "дурацкая"];
+    //     let randomWords = ["муха", "выдра", "дубина", "мартышка", "крыса"];
+    //     let randomInsult = "У тебя " + pickRandomWord(randomBodyParts) + " словно " + pickRandomWord(randomAdjectives) + ' ' + pickRandomWord(randomWords);
+    //     return randomInsult
+    //
+    // }
+    // console.log(generateRandomInsult())
+    // console.log(generateRandomInsult())
+    // console.log(generateRandomInsult())
+    //
+    // let fifthLetter = function (name) {
+    //     if (name.length < 5) {
+    //         return;
+    //     }
+    //     return "Пятая буква вашего имени: " + name[4] + ".";
+    // };
+    // console.log(fifthLetter('Ira'))
+    //
+    //
+    // let medalForScore = function (score) {
+    //     if(score < 3) {
+    //         return 'Бронзовая'
+    //     }
+    //     if(score < 7) {
+    //         return 'Серебряная'
+    //     }
+    //     return "Золотая"
+    // };
+    //
+    // console.log(medalForScore(5))
+
+    // function double(number) {
+    //     return number * 2;
+    // }
+    // console.log(double(5) + double(6))
 
 
-    const string1 = 'hello world';
-    const string2 ='I like js';
-    let count = 0;
-    const h1= document.querySelector('.head');
+    /////////////////////////////////////////////////////////////////////////////////////////
 
-    setInterval(function () {
-        count++
-        if (count % 2){
-            h1.innerHTML=string1
-        }
-            else{
-            h1.innerHTML=string2
-        }
-    }, 1000);
+    // function add(numberOne, numberTwo){
+    //   return numberOne + numberTwo
+    // }
+    //
+    // function multiply(numberOne, numberTwo){
+    //  return numberOne * numberTwo
+    // }
+    // console.log(multiply(3726,add(15,26)))
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//     let numberArrayOne = [1, 2, 5];
+//     let numberArrayTwo = [1, 2, 5];
+//
+//     let areArraysSame = function (numberArrayOne, numberArrayTwo) {
+//         if (numberArrayOne.length !== numberArrayTwo.length) {
+//             return false
+//         }
+//
+//         for (let i = 0; i < numberArrayOne.length; i++) {
+//             if (numberArrayOne[i] !== numberArrayTwo[i]) {
+//                 return false
+//             }
+//         }
+//
+//         return true
+//     };
+//
+//     console.log(areArraysSame(numberArrayOne, numberArrayTwo))
+
+
+
 });
