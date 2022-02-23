75a8728f423e20294dda10d517e117c8d6265c04
Ira Redchets <irared95@gmail.com>
2022-02-24 00:01:56 +0200

    js practice chapter 12 OOP

--- main.js
+++ main.js
@@ -1,18 +1,120 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+//     let dog = {
+//         name: "Оладушек",
+//         legs: 4,
+//         isAwesome: true
+//     };
+//     dog.age = 6;
+//     console.log(dog.name)
+// console.log(dog)
+//
+//     let speak = function () {
+//         console.log(this.sound + "! Меня зовут " + this.name + "!");
+//     };
+//     let cat = {
+//         sound: "Мяу",
+//         name: "Варежка",
+//         speak: speak
+// };
+//     console.log(cat.speak())
+//
+//     let pig = {
+//         sound: "Хрю",
+//         name: "Чарли",
+//         speak: speak
+//     };
+//    let horse = {
+//         sound: "И-го-го",
+//         name: "Мэри",
+//         speak: speak
+//     };
+//     pig.speak();
+//     horse.speak();
 
-    const string1 = 'hello world';
-    const string2 ='I like js';
-    let count = 0;
-    const h1= document.querySelector('.head');
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    let Car = function (x, y) {
+        this.x = x;
+        this.y = y;
+        this.speed = 15;
+        this.speedRandom = () => Math.floor(Math.random() * 11);
+    };
+
+
+
+    Car.prototype.draw = function () {
+        let carHtml = '<img alt="car" src="./image/car-red.webp">';
+        //передаем carHTML в функцию $, которая преобразует HTML в jQuery элемент
+        this.carElement = $(carHtml);
+        //мы вызываем для carElement метод css, чтобы задать изображению машины координаты, стили
+        this.carElement.css({
+            position: "absolute",
+            left: this.x,
+            top: this.y,
+            backgroundColor: 'red',
+            border: '4px solid blue',
+        });
+        //с помощью jQuery добавляем carElement к элементу body нашего HTML-документа
+      $("body").append(this.carElement);
+    };
+
+    Car.prototype.moveRight = function () {
+        if (this.x > 300){
+            return false
+        }
+        this.x += this.speedRandom();
+        this.carElement.css({
+            left: this.x,
+            top: this.y
+        });
+        console.log(this.speedRandom())
+    }
+
+    Car.prototype.moveLeft = function () {
+        this.x -= 5;
+        this.carElement.css({
+            left: this.x,
+            top: this.y
+        });
+    };
+    Car.prototype.moveUp = function () {
+        this.y -= 5;
+        this.carElement.css({
+            left: this.x,
+            top: this.y
+        });
+    };
+    Car.prototype.moveDown = function () {
+        this.y += 5;
+        this.carElement.css({
+            left: this.x,
+            top: this.y
+        });
+    };
+
+    let tesla = new Car(10, 20);
+    let nissan = new Car(10, 200);
+
+    tesla.draw();
+    nissan.draw();
+
+    tesla.moveLeft()
+    tesla.moveUp()
+    tesla.moveDown()
+
+
 
     setInterval(function () {
-        count++
-        if (count % 2){
-            h1.innerHTML=string1
-        }
-            else{
-            h1.innerHTML=string2
-        }
-    }, 1000);
+        nissan.moveRight()
+    }, 300)
+
+    setInterval(function () {
+        tesla.moveRight()
+    }, 300)
+
+
+
+
+
 });
