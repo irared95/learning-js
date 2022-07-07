9de55900f12946aac46dc26c157dd82961b171b7
Ira Redchets <irared95@gmail.com>
2022-07-07 13:47:15 +0300

    cleaning files

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+
+    //функция принимапт два числа
+    //если оба четные то возвращает произведение
+    //если не четные сумму
+    //если одно число четное а другое нет то возращает не четное
+
+    // function getNum (a,b){
+    //     if (a % 2 === 0 && b % 2 === 0){
+    //         return a * b
+    //     } else if (a % 2 !== 0 && b % 2 !== 0) {
+    //         return a + b
+    //     } else {
+    //       return a || b !== 0
+    //     }
+    // }
+    // console.log(getNum(3,10))
 
 
+    //найти вес всех продуктов цена которых более 30 и количество менее 7
+    // const food = [
+    //     {name: 'sausage', weight: 350, price: 11, quantity: 7},
+    //     {name: 'cabbage', weight: 100, price: 32, quantity: 5},
+    //     {name: 'apple', weight: 230, price: 45, quantity: 5},
+    //     {name: 'tomato', weight: 150, price: 34, quantity: 13},
+    //     {name: 'lemon', weight: 50, price: 12, quantity: 4},
+    // ]
+    //
+    //
+    // let foodFilter = food
+    //     .filter(elem => elem.price > 30 && elem.quantity < 7)
+    //     .reduce((acc,elem) => acc + elem.weight, 0)
+    // console.log(foodFilter)
 });
