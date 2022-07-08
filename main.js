6130e799de5a3a45bf54d6314bd762aaf6b17e96
Ira Redchets <irared95@gmail.com>
2022-07-08 15:37:59 +0300

    done accordion

--- main.js
+++ main.js
@@ -1,34 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    //функция принимапт два числа
-    //если оба четные то возвращает произведение
-    //если не четные сумму
-    //если одно число четное а другое нет то возращает не четное
+    const accordionItem = $('.accordion__item');
 
-    // function getNum (a,b){
-    //     if (a % 2 === 0 && b % 2 === 0){
-    //         return a * b
-    //     } else if (a % 2 !== 0 && b % 2 !== 0) {
-    //         return a + b
-    //     } else {
-    //       return a || b !== 0
-    //     }
-    // }
-    // console.log(getNum(3,10))
+    accordionItem.on('click',function(event){
+        //this текущие нажатие
+        const content = $(this).find('.accordion__item-content')
 
+        $('.accordion__item-content').not(content).slideUp(500).removeClass('active')
 
-    //найти вес всех продуктов цена которых более 30 и количество менее 7
-    // const food = [
-    //     {name: 'sausage', weight: 350, price: 11, quantity: 7},
-    //     {name: 'cabbage', weight: 100, price: 32, quantity: 5},
-    //     {name: 'apple', weight: 230, price: 45, quantity: 5},
-    //     {name: 'tomato', weight: 150, price: 34, quantity: 13},
-    //     {name: 'lemon', weight: 50, price: 12, quantity: 4},
-    // ]
-    //
-    //
-    // let foodFilter = food
-    //     .filter(elem => elem.price > 30 && elem.quantity < 7)
-    //     .reduce((acc,elem) => acc + elem.weight, 0)
-    // console.log(foodFilter)
+        if (content.hasClass('active')){
+            content.removeClass('active')
+            content.slideUp(500)
+        } else {
+            content.addClass('active')
+            content.slideDown(500)
+        }
+    })
+
+    console.log(accordionItem)
+
 });
